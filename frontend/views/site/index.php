a661f2e81920357bd66693fac819af04aee9ace9
malika <malikarabbay@gmail.com>
2016-07-08 17:59:17 +0600

    main rss nur.kz

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -23,7 +23,7 @@ use tugmaks\RssFeed\RssReader;
         </p>
         <p class="compani_text">The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
     </div>
-  
+
 </div>
 
 
