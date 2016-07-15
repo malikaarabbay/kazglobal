cec316445759a844b31f0d4e0863da5490018304
nurzhik <nurzhanbalkibek@yandex.ru>
2016-07-15 15:49:22 +0600

    Новости адаптивость

--- frontend/views/news/index.php
+++ frontend/views/news/index.php
@@ -14,26 +14,7 @@ $this->registerMetaTag(['name'=> 'description', 'content' => '']);
 
 ?>
 
-<<<<<<< HEAD
 
-    <div class="containers">
-        <div class="content">
-             <div class="cr">
-             <div class="title text-decoration">
-                     <h1><?= $this->title ?></h1>
-                 </div>
-            <?php echo RssReader::widget([
-                'channel' => 'https://www.nur.kz/rss/all.rss',
-                'pageSize' => 10,
-                'itemView' => '@frontend/views/news/_item', //To set own viewFile set 'itemView'=>'@frontend/views/site/_rss_item'. Use $model var to access item properties
-                'wrapTag' => 'div',
-                'wrapClass' => 'rss-wrap',
-            ]);
-            ?>
-             </div>
-        </div>
-    </div>
-=======
+
 <?= $this->render( '_item' )?>
 
->>>>>>> 59de4b84a32e4a702b1b901f89135c7ba4f77f97
